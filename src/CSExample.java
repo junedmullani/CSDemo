21a77eb72a3ccf319534295aa5b20a5830f163f0
JUNED <JUNED@platform.wondershare.com>
2020-09-02 16:30:01 +0530

    CSDEMO Commit

---
+++ src/CSExample.java
@@ -0,0 +1,17 @@
+
+public class CSExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Employee e=new Employee("abc",18);
+	Employee e1=new Employee("abc",19);
+	Employee e2=new Employee("abc",20);
+	
+	Employee eArray[]=new Employee[5];
+	eArray[0]=e;
+	eArray[1]=e1;
+	eArray[2]=e2;
+	
+	
+	}
+}
